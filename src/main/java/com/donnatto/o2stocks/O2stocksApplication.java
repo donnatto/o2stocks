6ec2022c5e23783ebb049e211c042196384bbd7b
Edwin Dominguez <edwindominguez@hotmail.com>
2020-05-06 21:32:36 -0500

    first commit

---
+++ src/main/java/com/donnatto/o2stocks/O2stocksApplication.java
@@ -0,0 +1,13 @@
+package com.donnatto.o2stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2stocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2stocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donnatto/o2stocks/O2stocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donnatto.o2stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2stocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
